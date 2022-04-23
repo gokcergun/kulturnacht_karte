8a224dbe5ba1f046da8dee58f016fd80d8d1ab08
Gokce Ergun <gokcergun@gmail.com>
2022-04-23 12:04:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ data = pd.read_csv('Data/orts.csv',encoding='cp1252')
 #create map
 m = folium.Map(location=[48.520462436253766, 9.053572912482348], zoom_start=14)
 tooltip = "Klicken Sie hier für Informationen"
-iframe_start = folium.IFrame("""<b>18:00 Marktplatz</b><br> Startschuss  der 8. Tübinger Kulturnacht
+iframe_start = folium.IFrame("""<b>18:00 Marktplatz</b><br> Startschuss  der Tübinger Kulturnacht
 <br><a href=https://www.kulturnacht-tuebingen.de/partys/location/location_id/8349 target="_blank">siehe Veranstaltungen</a>""")
 popup_start = folium.Popup(iframe_start, min_width=300, max_width=300, min_height=75, max_height=75)
 
@@ -57,7 +57,7 @@ for i in range(len(data)):
         popup_reg = folium.Popup(iframe_reg, min_width=300, max_width=300, min_height=75, max_height=75)
         # star marker
         icon_star = BeautifyIcon(icon='map-pin',
-                             inner_icon_style='color:#17b4e4;font-size:25px;', 
+                             inner_icon_style='color:#1bbbe9;font-size:25px;', 
                              background_color='transparent',border_color='transparent')
         marker = folium.Marker(
         location=location, icon=icon_star,
